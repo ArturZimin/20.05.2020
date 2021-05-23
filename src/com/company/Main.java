6d1f90b3b8136b7f9d76c73cb762da8619f51281
Artur <artrikris50@gmail.com>
2021-05-23 20:24:32 +0300

    Task6-11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //task10
+
+
+
+        //task10 Имеется 2 дома и участок,определить вместятся ли дома на участке
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("размеры дома №1");
@@ -31,15 +34,13 @@ public class Main {
             System.out.println("дома слишком большие и не помещаются на участке");
         }
 
-///рорордлбтд
 
-
-        //task8
+        //task8 Задать целое число в виде переменной и вывести на экран ,добавив название валюты
 
         int many = 100;
         System.out.println(many + " " + "dollars");
 
-        //task 9
+        //task 9  Задать 3 числа -день,месяц,год.Вывести на экран в виде трех чисел, дату следуещего дня
 
         int day = 20;
         int month = 05;
@@ -48,9 +49,13 @@ public class Main {
         System.out.println(++day + " " + zero + month + " " + year);
 
 
-//task7
         isRectangle(8);
+
+        weekSchedule(3);
     }
+
+    //task7 имеется прямоугольное отверстие разм. а и б. Где а и б целые числа.Определить можно ли его закрыть
+    //круглой картонкой радиусом р(тоже целое число)
 
     public static boolean isRectangle(int number) {
         int height = 4;
@@ -68,6 +73,71 @@ public class Main {
             return a;
         }
 
+
+    }
+
+
+    //Task11 Написать метод, который выводит расписание на неделю
+
+    public static void weekSchedule(int day) {
+
+
+        switch (day) {
+            case 1: {
+                System.out.println("Monday lessons schedule:");
+            }
+            break;
+            case 2: {
+                System.out.println("Tuesday lessons schedule:");
+            }
+            break;
+            case 3: {
+                System.out.println("Wednesday lessons schedule:");
+            }
+            break;
+
+            case 4: {
+                System.out.println("Thursday lessons schedule:");
+            }
+            break;
+            case 5: {
+                System.out.println("Friday lessons schedule: ");
+            }
+            break;
+            case 6: {
+                System.out.println("Holiday saturday");
+            }
+            break;
+            case 7: {
+                System.out.println("Holiday sunday");
+            }
+            break;
+            default: {
+                System.out.println("Day of week input error");
+            }
+
+
+        }
+
+        isSeven(237);
+
+    }
+
+    //Task6
+
+    //Создайте метод с одним целочисленным параметром .Метод должен определить ,
+    //является ли последняя цифра 7 и вернуть значение boolean
+    public static boolean isSeven(int number) {
+
+        boolean a = true;
+        boolean b = false;
+        if (number % 10 == 7) {
+            System.out.println(a);
+            return a;
+        } else {
+            System.out.println(b);
+            return b;
+        }
     }
 }
 
